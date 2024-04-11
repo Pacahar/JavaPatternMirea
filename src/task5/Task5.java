6634049c32a926be78dadaa4d79a19244b5cc992
Pacahar <s.kirill29@gmail.com>
2024-04-11 17:13:21 +0300

    change 5 task

--- src/task5/Task5.java
+++ src/task5/Task5.java
@@ -2,11 +2,22 @@ package task5;
 
 public class Task5 {
     public static void main(String[] args) {
-        SecondSingleton f = SecondSingleton.INSTANCE;
-        f.value = 10;
-        SecondSingleton s = SecondSingleton.INSTANCE;
-        System.out.println(f.value);
-        System.out.println(s.value);
+        FirstSingleton ff = FirstSingleton.getInstance();
+        ff.value = 10;
+        FirstSingleton fs = FirstSingleton.getInstance();
+        System.out.println(ff.value);
+        System.out.println(fs.value);
+
+        SecondSingleton sf = SecondSingleton.INSTANCE;
+        sf.value = 20;
+        SecondSingleton ss = SecondSingleton.INSTANCE;
+        System.out.println(sf.value);
+        System.out.println(ss.value);
+
+        ThirdSingleton tf = ThirdSingleton.getInstance(30);
+        ThirdSingleton ts = ThirdSingleton.getInstance();
+        System.out.println(tf.getValue());
+        System.out.println(ts.getValue());
 
     }
 }
